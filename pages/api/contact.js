d377fb5aa1038dec8f42e3f2c13b84f0e4aa976d
Nick <nickdehart@gmail.com>
2019-12-15 16:39:32 -0500

    updated api's to use env, updated start scripts

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,18 +1,17 @@
 const MongoClient = require('mongodb').MongoClient;
 const sanitize = require('mongo-sanitize');
-const config = require('../../config');
 
 export default (req, res) => {
    if (req.method === 'POST') {
-      MongoClient.connect(config.default.mongo.url, (err, client) => {
+      MongoClient.connect(process.env.DB_HOST, (err, client) => {
          if(err) {
             res.status(400).send({
                message: err
             });
          }
 
-         const db = client.db(config.default.mongo.db);
-         const collection = db.collection(config.default.mongo.collections.contact);
+         const db = client.db(process.env.DB_NAME);
+         const collection = db.collection(process.env.DB_CONTACT);
          try {
             Object.keys(req.body).forEach(param => {
                req.body[param] = sanitize(req.body[param])
