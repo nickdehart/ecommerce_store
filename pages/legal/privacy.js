2e362554108185ab5cac318ae754eaed5d052b27
Nick <nickdehart@gmail.com>
2020-01-01 20:24:10 -0500

    removed shopify and other stuff from privacy and terms text

--- pages/legal/privacy.js
+++ pages/legal/privacy.js
@@ -3,7 +3,7 @@ const Privacy = ({config}) => (
   <div>
    <h1 className="title add-margin">Privacy Policy</h1>
    <div className="container add-margin">
-      This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase at Get Ur Tools Out.
+      This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase.
       <br />
       <br /> <strong>Personal information we collect</strong>
       <br /> When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information”.
@@ -26,7 +26,7 @@ const Privacy = ({config}) => (
       <br /> We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).
       <br />
       <br /> <strong>Sharing your personal Information</strong>
-      <br /> We share your Personal Information with third parties to help us use your Personal Information, as described above. For example, we use Shopify to power our online store--you can read more about how Shopify uses your Personal Information here: https://www.shopify.com/legal/privacy. We also use Google Analytics to help us understand how our customers use the Site -- you can read more about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.
+      <br /> We share your Personal Information with third parties to help us use your Personal Information, as described above. We use Google Analytics to help us understand how our customers use the Site -- you can read more about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.
       <br />
       <br /> Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.
       <br />
--- pages/legal/terms.js
+++ pages/legal/terms.js
@@ -13,8 +13,6 @@ const Terms = ({config}) => (
       <br />
       <br /> Any new features or tools which are added to the current store shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.
       <br />
-      <br /> Our store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you.
-      <br />
       <br /> <strong>SECTION 1 - ONLINE STORE TERMS</strong>
       <br /> By agreeing to these Terms of Service, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.
       <br /> You may not use our products for any illegal or unauthorized purpose nor may you, in the use of the Service, violate any laws in your jurisdiction (including but not limited to copyright laws).
